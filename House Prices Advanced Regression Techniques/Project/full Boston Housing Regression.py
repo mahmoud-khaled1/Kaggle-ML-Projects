63ff7e8c6139a7965458fc9b65d39e7fe101e5f2
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2020-10-19 22:05:47 +0200

    add

--- House Prices Advanced Regression Techniques/Project/full Boston Housing Regression.py
+++ House Prices Advanced Regression Techniques/Project/full Boston Housing Regression.py
@@ -6,21 +6,19 @@ from scipy.stats import norm
 from sklearn.preprocessing import StandardScaler
 from scipy import stats
 
-#Attach Data Sets
+#1-Data File and attach data
 Train=pd.read_csv('D:\\train.csv')
 Test=pd.read_csv('D:\\test.csv')
-
 
 """
 print("Price Details:")
 print(Train['SalePrice'].describe())
-"""
+
 #histogram
-"""
+
 sns.distplot(Train['SalePrice'])
 plt.show()
 """
-
 #scatter plot grlivarea(Area Of House)/saleprice
 data = pd.concat([Train['SalePrice'], Train['GrLivArea']], axis=1) #Concat 2 column together to plot them
 data.plot.scatter(x='GrLivArea', y='SalePrice', ylim=(0,800000));
@@ -30,26 +28,10 @@ data.plot.scatter(x='GrLivArea', y='SalePrice', ylim=(0,800000));
 data = pd.concat([Train['SalePrice'], Train['TotalBsmtSF']], axis=1) #Concat 2 column together to plot them
 data.plot.scatter(x='TotalBsmtSF', y='SalePrice', ylim=(0,800000));
 
-
-
-#box plot overallqual/saleprice #Min and max price of overallQuality of house
-data = pd.concat([Train['SalePrice'], Train['OverallQual']], axis=1)
-f, ax = plt.subplots(figsize=(8, 6))
-fig = sns.boxplot(x='OverallQual', y="SalePrice", data=data)
-fig.axis(ymin=0, ymax=800000);
-
-#box plot YearBuilt/saleprice #Min and max price of YearBuilt of house
-data = pd.concat([Train['SalePrice'], Train['YearBuilt']], axis=1)
-f, ax = plt.subplots(figsize=(16, 8))
-fig = sns.boxplot(x='YearBuilt', y="SalePrice", data=data)
-fig.axis(ymin=0, ymax=800000)
-plt.xticks(rotation=90)
-
 #correlation matrix ---> Relationship between every column and each
 corrmat = Train.corr()
 f, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=.8, square=True)
-
 
 #saleprice correlation matrix of  best specific column(Feature) with  precient number
 k = 10 #number of variables for heatmap
@@ -59,26 +41,10 @@ sns.set(font_scale=1.25)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
 
 
-#scatterplot
-sns.set()
-cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
-sns.pairplot(Train[cols], size = 2.5)
-
+#2-Data Cleaning
 
 #missing data : dealing with null  in column because if we train algorithm with them,
 #then miss leading will occured.
-
-#We'll consider that when more than 15% of the data is missing, we should delete the corresponding variable
-# and pretend it never existed. This means that we will not try any trick to fill the missing data in these cases.
-# According to this, there is a set of variables (e.g. 'PoolQC', 'MiscFeature', 'Alley', etc.)
-# that we should delete. The point is: will we miss this data? I don't think so.
-# None of these variables seem to be very important, since most of them are not aspects in which
-# we think about when buying a house (maybe that's the reason why data is missing?).
-# Moreover, looking closer at the variables, we could say that variables like 'PoolQC',
-# 'MiscFeature' and 'FireplaceQu' are strong candidates for outliers, so we'll be happy to delete them.
-#In summary, to handle missing data, we'll delete all the variables with missing data, except the variable 'Electrical'.
-# In 'Electrical' we'll just delete the observation with missing data.
-#Print these missing  data column
 total = Train.isnull().sum().sort_values(ascending=False)
 percent = (Train.isnull().sum()/Train.isnull().count()).sort_values(ascending=False)
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
@@ -86,20 +52,14 @@ print(missing_data.head(20))
 
 #dealing with missing data
 Train = Train.drop((missing_data[missing_data['Total'] > 1]).index,1)
-Train = Train.drop(Train.loc[Train['Electrical'].isnull()].index)
+Train = Train.drop(Train.loc[Train['Electrical'].isnull()].index) #remove this row in electrical where have null value .
 print('*'*50)
 print("No of Missing Data :",Train.isnull().sum().max()) #just checking that there's no missing data missing...
 
-#Data Scaling
-#standardizing data
-saleprice_scaled = StandardScaler().fit_transform(Train['SalePrice'][:,np.newaxis]);
-low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10] #rearrange Ascending
-high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:] #rearrange Descending
-print('outer range (low) of the distribution:')
-print(low_range)
-print('\nouter range (high) of the distribution:')
-print(high_range)
+#3-Data Scaling
+#saleprice_scaled = StandardScaler().fit_transform(Train['SalePrice'][:,np.newaxis]);
 
+#print(saleprice_scaled[:10])
 #bivariate analysis between saleprice/grlivarea after Data Scaling
 var = 'GrLivArea'
 data = pd.concat([Train['SalePrice'], Train[var]], axis=1)
@@ -111,43 +71,60 @@ data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
 # Therefore, we'll define them as outliers and delete them.
 
 #deleting points
-Train.sort_values(by = 'GrLivArea', ascending = False)[:2]
+#Train.sort_values(by = 'GrLivArea', ascending = False)[:2]
 Train = Train.drop(Train[Train['Id'] == 1299].index)
 Train = Train.drop(Train[Train['Id'] == 524].index)
 
-#bivariate analysis between saleprice/grlivarea after Delete those strange point (anonymous point)
-data = pd.concat([Train['SalePrice'], Train['TotalBsmtSF']], axis=1)
-data.plot.scatter(x='TotalBsmtSF', y='SalePrice', ylim=(0,800000));
-
-
-#bivariate analysis saleprice/grlivarea
-var = 'TotalBsmtSF'
+#bivariate analysis between saleprice/grlivarea after Delete these points
+var = 'GrLivArea'
 data = pd.concat([Train['SalePrice'], Train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
-#**We can feel tempted to eliminate some observations (e.g. TotalBsmtSF > 3000)
-# but I suppose it's not worth it. We can live with that, so we'll not do anything.
 
+Train2 = Train.drop("Id", axis=1)
+Y=Train2.iloc[:,-1:]
+Train2 = Train2.drop("SalePrice", axis=1)
 #convert categorical(string input in column ) variable into dummy
 #make new columns  and put integer value as classification target
-Train = pd.get_dummies(Train)
+Train2 = pd.get_dummies(Train2)
 
+"""
+from sklearn.preprocessing import StandardScaler
+#Copy= True Just told algorithm don't change the data
+Data_Scaling =StandardScaler(copy=True,with_mean=True,with_std=True)#.fit_transform(data)
+Scaling_data=Data_Scaling.fit_transform(Train2)
+"""
+X=Train2.iloc[:,:]
 
+#print(X.shape)
+#print(Y.shape)
 
-#Applying algorithms of Regression ...........
+#4-Data Splitting
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.23,shuffle=True,random_state=44)
 
+#Apply Linear Regression algorithm :
 from sklearn.linear_model import LinearRegression
-Linear_Reg_model=LinearRegression(fit_intercept=True,normalize=True ,copy_X=True,n_jobs=-1)
-Linear_Reg_model.fit(Train,)
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import median_absolute_error
 
+# fit_intercept : هل نريد حساب نقطه التقاطع مع محور اكس ام لا
+# normalize : make normalization to data or not
+#copy_x : if you want to just copy only the data without any change of them
+#n_jobs : is  specify speed of operation in your processor if =0 or none will be normal ,-1 is faster speed of processor
+# and the more than 1 the more than speed
+Linear_Reg_model=LinearRegression(fit_intercept=True,normalize=True ,copy_X=True,n_jobs=-1)
+Linear_Reg_model.fit(X_train,y_train)
 
 #Show Details
 #Note the best Score is 1 so we should make it near to 1
 print("Linear Regression Train Score :",Linear_Reg_model.score(X_train,y_train))
 print("Linear Regression test Score :",Linear_Reg_model.score(X_test,y_test))
-#print("Linear Regression Coef Score :",Linear_Reg_model.coef_)
-#print("Linear Regression intercept Score :",Linear_Reg_model.intercept_)
 
-Y_pre=Linear_Reg_model.predict(X_test)
+#Calculating Predication
+Y_pred=Linear_Reg_model.predict(X_test)
+print(Y_pred[:10])
+print(y_test[:10])
 
-print(list(Y_pre[:10]))
-print(list(y_test[:10]))
+
+
