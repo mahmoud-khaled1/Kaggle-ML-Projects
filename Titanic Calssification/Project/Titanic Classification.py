392bd36343fec83cbb64eeb5c92e5abc73cdffd8
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2020-11-05 15:51:22 +0200

    add

--- Titanic Calssification/Project/Titanic Classification.py
+++ Titanic Calssification/Project/Titanic Classification.py
@@ -64,7 +64,161 @@ sns.heatmap(corrmat, vmax=.8, square=True)
 print("Before", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
 train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
 test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
-combine = [train_df, test_df]
+combine = [train_df, test_df] # train and test together
 print("After", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
 
+#Creating new feature extracting from existing
+#We want to analyze if Name feature can be engineered to extract titles
+# and test correlation between titles and survival, before dropping Name and PassengerId features.
+#In the following code we extract Title feature using regular expressions.
+# The RegEx pattern (\w+\.) matches the first word which ends with a dot character
+# within Name feature. The expand=False flag returns a DataFrame.
+
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False) #create new column and add it to dataset in train and test
+
+#To show title
+#print(pd.crosstab(train_df['Title'], train_df['Sex']))
+
+#We can replace many titles with a more common name or classify them as Rare.
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', \
+                                                 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+#to show mean of every title
+#print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+#We can convert the categorical titles to ordinal.
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+train_df.head()
+
+#Now we can safely drop the Name feature from training and testing datasets.
+# We also do not need the PassengerId feature in the training dataset.
+train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
+test_df = test_df.drop(['Name'], axis=1)
+combine = [train_df, test_df]
+print(train_df.shape, test_df.shape)
+
+#Converting a categorical feature sex
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+#print(train_df.head())
+
+#Let us start by preparing an empty array to contain guessed Age values based on Pclass x Gender combinations.
+#Now we iterate over Sex (0 or 1) and Pclass (1, 2, 3) to calculate guessed values of Age for the six combinations.
+guess_ages = np.zeros((2,3))
+
+for i in range(0, 2):
+    for j in range(0, 3):
+        guess_df = dataset[(dataset['Sex'] == i) & \
+                           (dataset['Pclass'] == j + 1)]['Age'].dropna()
+
+        # age_mean = guess_df.mean()
+        # age_std = guess_df.std()
+        # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+
+        age_guess = guess_df.median()
+
+        # Convert random age float to nearest .5 age
+        guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
+
+for i in range(0, 2):
+    for j in range(0, 3):
+        dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), \
+                    'Age'] = guess_ages[i, j]
+dataset['Age'] = dataset['Age'].astype(int)
+
+#Let us create Age bands and determine correlations with Survived.
+train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
+
+# replace Age with  based on range of people be(0,1,2,3) only
+for dataset in combine:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age']
+
+#We can create a new feature for FamilySize which combines Parch and SibSp.
+# This will enable us to drop Parch and SibSp from our datasets.
+for dataset in combine:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+#We can create another feature called IsAlone.
+
+for dataset in combine:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+
+#Let us drop Parch, SibSp,  features in favor of IsAlone.
+
+train_df = train_df.drop(['Parch', 'SibSp'], axis=1)
+test_df = test_df.drop(['Parch', 'SibSp'], axis=1)
+combine = [train_df, test_df]
+
+#TO FILL Embarked COLUMN WITH VALUE IN CASE OF NULL
+#LET'S GET MOST FREQUANCY
+freq_port = train_df.Embarked.dropna().mode()[0]
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+
+train_df[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean().sort_values(by='Survived',
+                                                                                            ascending=False)
+#Converting categorical feature to numeric¶
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+#Fill null fare with mean values
+#We can now complete the Fare feature for single missing value in test dataset using mode
+# to get the value that occurs most frequently for this feature. We do this in a single line of code.
+
+test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+
+train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True)
+#Convert the Fare feature to ordinal values based on the FareBand.
+
+for dataset in combine:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+train_df = train_df.drop(['FareBand'], axis=1)
+combine = [train_df, test_df]
+
+#3-Feature selection
+#I Need Every Feature
+
+#4-Data Scaling
+#No need To scale data
+
+#5-Data Splitting
+X_train = train_df.drop("Survived", axis=1)
+Y_train = train_df["Survived"]
+X_test  = test_df.drop("PassengerId", axis=1).copy()
+#print(X_train.shape, Y_train.shape, X_test.shape)
+
+#6-Choice best algorithm :
+"""
+Logistic Regression
+KNN or k-Nearest Neighbors
+Support Vector Machines
+Naive Bayes classifier
+Decision Tree
+Random Forrest
+Perceptron
+Artificial neural network
+RVM or Relevance Vector Machine
+"""
 
