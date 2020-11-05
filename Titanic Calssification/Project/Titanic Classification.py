209e63f9e3e40ba2fa5557c131e03d992b031477
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2020-11-05 17:15:30 +0200

    add

--- Titanic Calssification/Project/Titanic Classification.py
+++ Titanic Calssification/Project/Titanic Classification.py
@@ -16,6 +16,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 #1-Data File and attach DataSets
 train_df=pd.read_csv(r'D:\\Git_Projects\\Kaggle-ML-Projects\\Titanic Calssification\\train.csv')
@@ -115,30 +117,30 @@ for dataset in combine:
 #Let us start by preparing an empty array to contain guessed Age values based on Pclass x Gender combinations.
 #Now we iterate over Sex (0 or 1) and Pclass (1, 2, 3) to calculate guessed values of Age for the six combinations.
 guess_ages = np.zeros((2,3))
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & \
+                               (dataset['Pclass'] == j + 1)]['Age'].dropna()
 
-for i in range(0, 2):
-    for j in range(0, 3):
-        guess_df = dataset[(dataset['Sex'] == i) & \
-                           (dataset['Pclass'] == j + 1)]['Age'].dropna()
+            # age_mean = guess_df.mean()
+            # age_std = guess_df.std()
+            # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
 
-        # age_mean = guess_df.mean()
-        # age_std = guess_df.std()
-        # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+            age_guess = guess_df.median()
 
-        age_guess = guess_df.median()
+            # Convert random age float to nearest .5 age
+            guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
 
-        # Convert random age float to nearest .5 age
-        guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
+    for i in range(0, 2):
+        for j in range(0, 3):
+            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), \
+                        'Age'] = guess_ages[i, j]
 
-for i in range(0, 2):
-    for j in range(0, 3):
-        dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), \
-                    'Age'] = guess_ages[i, j]
-dataset['Age'] = dataset['Age'].astype(int)
-
+    dataset['Age'] = dataset['Age'].astype(int)
 #Let us create Age bands and determine correlations with Survived.
-train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
-train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
+#train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+#train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
 
 # replace Age with  based on range of people be(0,1,2,3) only
 for dataset in combine:
@@ -147,6 +149,10 @@ for dataset in combine:
     dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
     dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
     dataset.loc[ dataset['Age'] > 64, 'Age']
+
+
+
+
 
 #We can create a new feature for FamilySize which combines Parch and SibSp.
 # This will enable us to drop Parch and SibSp from our datasets.
@@ -204,9 +210,9 @@ combine = [train_df, test_df]
 #No need To scale data
 
 #5-Data Splitting
-X_train = train_df.drop("Survived", axis=1)
-Y_train = train_df["Survived"]
-X_test  = test_df.drop("PassengerId", axis=1).copy()
+X = train_df.drop("Survived", axis=1)
+Y = train_df["Survived"]
+x_test  = test_df.drop("PassengerId", axis=1).copy()
 #print(X_train.shape, Y_train.shape, X_test.shape)
 
 #6-Choice best algorithm :
@@ -221,4 +227,42 @@ Perceptron
 Artificial neural network
 RVM or Relevance Vector Machine
 """
+
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.3,shuffle=True,random_state=44)
+
+
+def Logistaccc_Calssification():
+    Logistic_Regression_model = LogisticRegression(penalty='l2', solver='liblinear', C=0.1, random_state=44, n_jobs=-1,
+                                                   max_iter=10000, l1_ratio=None, tol=1e-4, dual=False)
+    Logistic_Regression_model.fit(X_train, y_train)
+    # Calculation Details
+    print("Train Score :", Logistic_Regression_model.score(X_train, y_train))
+    print("test Score :", Logistic_Regression_model.score(X_test, y_test))
+    print("Classes are :", Logistic_Regression_model.classes_)
+    # We have max_iter=10000 but here will found max_itr =4059 iteration that mean that algorithm after 4059 iteration not necessary
+    print("Max Iteration :", Logistic_Regression_model.n_iter_)
+    print("*" * 100)
+
+    # Make Predicted
+    y_pre = Logistic_Regression_model.predict(X_test)
+
+    print(y_test[:15])
+    print(y_pre[:15])
+
+    # Calculted Coufusion matrix
+    # [TP     FP
+    # [FN     TN]
+    ##We should make TP and TN biggest and smallest FP and FN
+    CM = confusion_matrix(y_test, y_pre)
+    print("Confusion Matrix :")
+    print(CM)
+    # show all correlation between output and all features
+    coeff_df = pd.DataFrame(train_df.columns.delete(0))
+    coeff_df.columns = ['Feature']
+    coeff_df["Correlation"] = pd.Series(Logistic_Regression_model.coef_[0])
+
+    coeff_df.sort_values(by='Correlation', ascending=False)
+
+
+Logistaccc_Calssification()
 
